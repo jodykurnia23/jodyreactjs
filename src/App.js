28db34066039608ef8678af57dee2dee62faf562
jody_kurnia <jodykurnia2@gmail.com>
2023-01-03 14:43:49 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br />
+          Jody Ganteng banget sih 😁
         </p>
         <a
           className="App-link"
