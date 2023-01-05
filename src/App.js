5ddf8716ab7f0e70a4c3129b938542c7fd101396
jody_kurnia <jodykurnia2@gmail.com>
2023-01-05 08:29:08 +0700

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br />
-          Jody Ganteng
+          Jody
         </p>
         <a
           className="App-link"
